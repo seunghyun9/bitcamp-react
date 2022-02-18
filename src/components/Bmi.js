81c3eb85267a5230f411fc9b9f1b5425f2a64772
sungsuhan <swhanssu@gmail.com>
2022-02-18 17:42:50 +0900

    2022-02-18 17:42 수업

--- src/components/Bmi.js
+++ src/components/Bmi.js
@@ -4,21 +4,27 @@ export default function Bmi(){
     const [name, setName] = useState("");
     const [height, setHeight] = useState(0);
     const [weight, setWeight] = useState(0);
-    return <Layout>
-    <h1>
-    BMI 측정기
-    </h1>
-    <form>
-    <div></div>
+    const execute = () => {
+        let name = document.getElementById('name').value
+        console.log('이름 : ' + name)
+        let height = document.getElementById('height').value
+        console.log('키 : ' + height)
+        let weight = document.getElementById('weight').value
+        console.log('몸무게 : ' + weight)
+        setName(name)
+        setHeight(height)    
+        setWeight(weight)
+    }
+    return <Layout><h1>BMI 측정기</h1>
     <div>
     <label><b>Username</b></label><br/>
-    <input></input><br/>
+    <input id="name"/><br/>
     <label><b>height(cm)</b></label><br/>
-    <input></input><br/>
+    <input id="height"/><br/>
     <label><b>weight(kg)</b></label><br/>
-    <input></input><br/>
-    <button>BMI 측정</button>
+    <input id="weight"/><br/>
+    <button onClick={() => {execute()}}>BMI 측정</button>
+    <div>결과: {name}, {height}cm, {weight}kg</div>
     </div>
-    </form>
     </Layout>
 }
--- src/components/Calc.js
+++ src/components/Calc.js
@@ -6,10 +6,16 @@ export default function Calc(){
     const [num2, setNum2] = useState(0)
     const [result, setResult] = useState(0)
     const sum = () => {
-        return setResult(Number(num1) + Number(num2))
+        let num1 = document.getElementById('num1').value
+        console.log('숫자1 : ' + num1)
+        let num2 = document.getElementById('num2').value
+        console.log('숫자2 : ' + num2)
+        setNum1(num1)
+        setNum2(num2)
+        setResult(Number(num1) + Number(num2))
+        console.log('결과 : ' + result)
     }
     return <Layout><h1>계산기</h1>
-    <div>
     <label><b>num1</b></label>
     <input id="num1" type="" /><br/>
     <label htmlFor=""><b>opcode</b></label>
@@ -23,10 +29,7 @@ export default function Calc(){
     <br/>
     <label htmlFor=""><b>num2</b></label>
     <input id="num2" tpye=""/><br/>
-    <button onClick={() => {setNum1(document.getElementById('num1').value)}}>num1 결정</button>
-    <button onClick={() => {setNum2(document.getElementById('num2').value)}}>num2 결정</button>
     <button onClick={() => {sum()}}>더하기 실행</button>
     <div>계산 결과: {num1} + {num2} = {result}</div>
-    </div>
     </Layout>
 }
